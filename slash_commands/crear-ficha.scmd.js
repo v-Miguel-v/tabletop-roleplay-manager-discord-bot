eb84b9b9462a3b22d04285b9be39643c73bb41fe
Miguel Velásquez <vmiguel.xy@gmail.com>
2023-08-31 22:27:24 -0400

    Corrección del .gitignore y formalización del comando "crear-ficha".
    Ahora el comando funciona~ Solo falta seguirlo perfeccionado ✨.

--- slash_commands/crear-ficha.scmd.js
+++ slash_commands/crear-ficha.scmd.js
@@ -10,15 +10,25 @@ module.exports = {
 			.setDescription("Canal de texto donde se guardará la ficha.")
 			.addChannelTypes(ChannelType.GuildText)
 			.setRequired(true)
+		)
+		.addUserOption(option => option
+			.setName("dueño")
+			.setDescription("Usuario al que le pertenecerá la ficha.")
 		),
 
 	async execute(interaction) {
+		const user = interaction.options.getUser("dueño") || interaction.user;
 		const channel = interaction.options.getChannel("canal");
 		const route = "./extras/character_sheet_formats/lewdity_quest_campaing";
+
+		channel.send(`**📝 __FICHA DE PERSONAJE DE ${user}__ 📝**`);
 		fs.readdirSync(route).forEach(file => {
-			const message = fs.readFileSync(`${route}/${file}`).toString("utf-8");
-			channel.send(message);
+			if (file.endsWith(".txt")) {
+				const message = fs.readFileSync(`${route}/${file}`).toString("utf-8");
+				channel.send(message);
+			}
 		});
-		await interaction.reply(`*Se ha creado una nueva ficha de personaje (vacía) en el canal ${channel}.*`);
+
+		await interaction.reply(`*Se ha creado una nueva ficha de personaje (vacía) para **${user.displayName}** en el canal ${channel}.*`);
 	}
 }
