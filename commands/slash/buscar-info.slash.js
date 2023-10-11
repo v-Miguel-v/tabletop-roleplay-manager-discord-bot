ee5a2de38efa4df8fec23d6756b6ca23a564e440
Miguel Velásquez <vmiguel.xy@gmail.com>
2023-10-11 13:12:03 -0400

    Resultados de la Barra de Búsqueda
    Se actualizó el comando /buscar-info y ahora en la barra de búsqueda aparezcen los resultados de 5etools (pero en español).
    El comando aún no responde con ninguna información, meramente envía un "Working" luego de que se definen los parámetros, pero el punto importante de esta actualización es que ahora la barra de búsqueda funciona y es capaz de encontrar resultados de entre los más de 7000 que se encuentran en los datos de 5etools.
    Aún sigue sin ser perfecto, y seguramente se pueda conseguir un resultado similar o incluso mejor de una forma más óptima reescribiendo el código de otra forma, pero por ahora... Funciona.
    
    Problemita Actual del Buscador: A pesar de que es capaz de buscar con bastante certeza a partir de los términos ingresados, debido a la forma en la que está escrito el código... No es capaz de encontrar palabras clave entre corchetes `[]` ni entre paréntesis `()`.
    Por ejemplo: Un resultado posible de entre los más de 7000 que hay en lo datos de 5etools es "[PHB] Acción: Esquivar (Dodge)", para encontrar ese resultado se puede escribir en la barra de búsqueda los siguientes inputs:
    - PHB Acción: Esquivar Dodge
    - Acción: Esquivar
    - Dodge
    - Esquivar
    - Esquivar Acción
    - Esquivar Dodge
    Entre otros. Combinaciones de todos los anteriores inputs son capaces de dar como resultado "[PHB] Acción: Esquivar (Dodge)". Sin embargo, si se intenta colocar el siguiente input: "[PHB] Acción: Esquivar" no se encontrará nada ya que se están utilizando corchetes `[]` y el algoritmo no toma en cuenta los corchetes para crear y busacar las palabras clave de los resultados (es decir, desde el punto de vista del algoritmo existe la palabra clave "PHB" y "Dodge" pero no las palabras clave "[PHB]" y "(Dodge)" así que al escribirlas de esa forma no dan resultados).
    Este es el pequeño problema que tiene actualmente el buscador. No es especialmente grave, pero habría que solucionarlo.
    
    Además, la forma de solucionarlo no debería ser simplemente agregar a las "palabras clave" de los resultados las opciones con corchetes y con paréntesis, sino que habría que especificar que "[PHB]" y "PHB" son equivalentes, y que si se coloca una, la otra se descarte, de no hacerlo de este modo búsquedas como "PHB [PHB] Acción: Esquivar Dodge (Dodge)" serían posibles y darían resultados cuando no deberían hacerlo (ya que "Dodge" y "(Dodge)" hacen referencia a la misma palabra clave y no son dos distintas. Lo mismo con respecto a "PHB" y "[PHB]").
    Pero en fin, por ahora funciona y está bien.

--- commands/slash/buscar-info.slash.js
+++ commands/slash/buscar-info.slash.js
@@ -1,12 +1,27 @@
 const fs = require("node:fs");
 const { SlashCommandBuilder } = require("discord.js");
+const databaseRoute = "./local_database";
+const globalCharRoute = `${databaseRoute}/character_sheet_types`;
 
 const trpgAvailable = [];
-fs.readdirSync("./local_database/character_sheet_types").forEach(trpg => {
-	if (fs.readdirSync(`./local_database/character_sheet_types/${trpg}`).includes("data-and-rules.json")) {
+fs.readdirSync(globalCharRoute).forEach(trpg => {
+	if (fs.readdirSync(`${globalCharRoute}/${trpg}`).includes("data-and-rules.js")) {
 		const trpgFormatted = trpg.replace(/_/g, " ");
 		trpgAvailable.push({ name: trpgFormatted, value: trpgFormatted });
 	}
+});
+
+const searchIndexes = {};
+trpgAvailable.forEach(trpg => {
+	const trpgDesformatted = trpg.name.replace(/ /g, "_");
+	const dataAndRulesUsedByTheTRPG = require(`../../${globalCharRoute}/${trpgDesformatted}/data-and-rules.js`);
+	searchIndexes[`${trpgDesformatted}_index`] = [];
+	dataAndRulesUsedByTheTRPG.forEach(dataset => {
+		const datasetRoute = `${databaseRoute}/data_and_rules/${dataset}_data`;
+		const data = fs.readFileSync(`${datasetRoute}/0-search-index-spanish.txt`, "utf-8").toString().split("\r\n").filter(x => x !== "");
+		const mergedData = searchIndexes[`${trpgDesformatted}_index`].concat(data);
+		searchIndexes[`${trpgDesformatted}_index`] = mergedData;
+	});
 });
 
 module.exports = {
@@ -27,10 +42,28 @@ module.exports = {
 		),
 
 	async autocomplete(interaction, client) {
-		const trpgSelected = interaction.options.getString("juego-de-rol");
+		const trpgSelected = interaction.options.getString("juego-de-rol").replace(/ /g, "_");
 		const input = interaction.options.getFocused().toLowerCase();
-		const choices = ["Clases", "Razas", "Reglas", "Items", "Bestiario", "Conjuros", "Dotes", "Armas", "Armaduras", "Trasfondo", "Referencia Rápida"]
-		const filtered = choices.filter(choice => choice.toLowerCase().startsWith(input));
+		let choices = searchIndexes[`${trpgSelected}_index`];
+		const filtered = choices.filter(choice => {
+			const regex = /[\s()\[\]]/g; // All Spaces, () and [].
+			const inputs = input.split(" ").filter(x => x !== "");
+			const words = choice.toLowerCase().split(regex).filter(x => x !== "");
+			// return words.some(word => word.startsWith(input)); // Versión Antigua
+			if (inputs.length > words.length) return false; // Si la cantidad de palabras ingresadas en el input es mayor a cualquier cantidad de palabras de los resultados, directamente no encontrará ningún resultado, así que retorna "false".
+			const copiedWords = [...words]; // Copiamos el array words ya que, para poder realizar adecuadamente el funcionamiento de la barra de búsqueda necesitaremos modificar y eliminar elmentos del array, así que para no alterar el original creamos una copia.
+			inputs.forEach(input => { // Para cada una de las palabras del input...
+				let indexToDelete = null; // 1. Inicializamos una variable "indexToDelete" en null.
+				if (copiedWords.some((word, index) => { // 2. (Condicional) Preguntamos: ¿En la lista de palabras (copiadas) de los resultados "words", hay alguna que empiece con la palabra actual del input?
+					indexToDelete = index; // 2.1 Guardamos el índice de la palabra "word" del resultado que hizo match con el input.
+					return word.startsWith(input); // 2.2 Verificamos la condición.
+				})) {
+					copiedWords.splice(indexToDelete, 1); // 3. Si la condición se cumple, antes de pasar a la siguiente palabra del input, borramos de la copia de la lista de palabras del resultado (copiedWords) la palabra con el índice que hizo match. Así, para la siguiente iteración, esa palabra no estará.
+				}
+			});
+			const allInputsMatched = (words.length - inputs.length) === (copiedWords.length); // Tras finalizar el forEach, y por tanto la evaluación del fragmento de código anterior para todas y cada una de las palabras del input... verificamos si todas las palabras hicieron Match mediante una resta. Si todas las palabras hicieron match, entonces a copiedWords se le borraron una cantidad de elementos iguales a la cantidad de elementos del input (porque precisamente los elementos del input eran los que se estaban iterando). En otras palabras: Si restamos de la cantidad de palabras del resultado (words) la cantidad de palabras del input (input), el resultado debería ser igual a la cantidad actual de elementos de la copia luego de la iteración si todas las palabras hicieron match.
+			return allInputsMatched ? true : false; // En caso de que sí, se retorna true, en caso de que no, se retorna false. Esta parte es algo redundante, pero es más que todo para darle más legibilidad y casi se pueda leer la frase "If all inputs matched... Then return true, if not, return false".
+		}).splice(0, 24);
 		await interaction.respond( filtered.map(choice => ({ name: choice, value: choice })) );
 	},
 
